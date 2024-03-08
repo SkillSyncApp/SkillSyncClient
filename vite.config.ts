e38b383a0bd51cf032bd8054ca09931552831d9e
amitbrickmanfa <amitbrickman@Amits-MacBook-Pro.local>
2024-03-08 17:46:55 +0200

    use https certs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync('./client-key.pem'),
+      cert: fs.readFileSync('./client-cert.pem'),
+    },
+  },
   plugins: [react()],
 })
