1282e2770855df33774582578c281e411d30d3bf
amitbrickmanfa <amitbrickman@Amits-MacBook-Pro.local>
2024-03-16 14:38:04 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
+// import fs from 'fs';
 
 export default defineConfig({
-  server: {
-    port: 443,
-    https: {
-      key: fs.readFileSync('../client-key.pem'),
-      cert: fs.readFileSync('../client-cert.pem'),
-    },
-  },
+  // server: {
+  //   port: 443,
+  //   https: {
+  //     key: fs.readFileSync('../client-key.pem'),
+  //     cert: fs.readFileSync('../client-cert.pem'),
+  //   },
+  // },
   base: "/",
   plugins: [react()],
 })
