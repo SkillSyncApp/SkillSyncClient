2eb05277e7e234212096870b45d38a554810b5ee
amitbrickmanfa <amitbrickman@Amits-MacBook-Pro.local>
2024-03-16 18:41:59 +0200

    base by env

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default ({ mode }) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    base: "https://node03.cs.colman.ac.il/public/client",
+    ...(process.env.VITE_ENV !== 'dev' && { base: "https://node03.cs.colman.ac.il/public/client" }),
     plugins: [react()],
     define: {
       "__REACT_APP_OPENAI_API_KEY__": `"${process.env.VITE_REACT_APP_OPENAI_API_KEY}"`,
