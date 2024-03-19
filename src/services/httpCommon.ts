d19f5dde90991a105a94e1dd97c084a2574f08e5
Hadar Zaguri <48456834+hadarza@users.noreply.github.com>
2024-03-19 23:07:01 +0200

    fix tokens issue (#70)

--- src/services/httpCommon.ts
+++ src/services/httpCommon.ts
@@ -14,8 +14,8 @@ apiClient.interceptors.response.use(
   },
   async (error) => {
     const originalRequest = error.config;
-    console.log(error.response)
-    if (error.response && error.response.status === 401 && error.response.data === "Unauthorized" && !originalRequest._retry) {
+    console.log(error.response.data)
+    if (error.response && error.response.data === "Unauthorized" && !originalRequest._retry) {
         await refreshTokens();
 
       try {
@@ -27,9 +27,6 @@ apiClient.interceptors.response.use(
         return Promise.reject(refreshError);
       }
     }
-    
-    // If the error is not due to an expired token, return the error
-    return Promise.reject(error);
   }
 );
 
